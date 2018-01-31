d01af796bb6d1129c44ece51c7c3405746fe1854
DiamondCrack <zk05161219@gmail.com>
2018-01-31 18:19:58 +0800

    login/regiseter view

--- src/container/login/Login.js
+++ src/container/login/Login.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import Logo from '../../components/logo/Logo'
+import {List,InputItem,WingBlank,WhiteSpace,Button} from 'antd-mobile'
 
 export default class Login extends Component {
+    register =()=>{
+        this.props.history.push('/register')
+    }
     render(){
         return(
             <div>
                 <Logo/>
+                <h2>登陆页面</h2>
+                <WingBlank>
+                    <List>
+                        <InputItem>
+                            用户
+                        </InputItem>
+                        <WhiteSpace/>
+                        <InputItem>
+                            密码
+                        </InputItem>
+                    </List>
+                    <WhiteSpace/>
+                    <Button type='primary'>登陆</Button>
+                    <WhiteSpace/>
+                    <Button onClick={this.register} type='primary'>注册</Button>
+                </WingBlank>
             </div>
         )
     }
--- src/container/register/Register.js
+++ src/container/register/Register.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
 import Logo from '../../components/logo/Logo'
+import {List,InputItem,WingBlank,WhiteSpace,Button,Radio} from 'antd-mobile'
 
 export default class Register extends Component{
+    state={
+        type:'seeker'
+    }
+    register =()=>{
+        this.props.history.push('/register')
+    }
     render(){
+        const {RadioItem} = Radio;
         return (
             <div>
                 <Logo/>
                 <h2>注册新用户</h2>
+                <List>
+                    <WhiteSpace/>
+                    <InputItem>用户名</InputItem>
+                    <WhiteSpace/>
+                    <InputItem>邮箱</InputItem>
+                    <WhiteSpace/>
+                    <InputItem>密码</InputItem>
+                    <WhiteSpace/>
+                    <InputItem>确认密码</InputItem>
+                    <WhiteSpace/>
+                    <RadioItem checked={this.state.type==='seeker'}>
+                        求职者
+                    </RadioItem>
+                    <WhiteSpace/>
+                    <RadioItem checked={this.state.type==='boss'}>
+                        BOSS
+                    </RadioItem>
+                    <Button type='primary'>注册</Button>
+                </List>
             </div>
         )
     }
